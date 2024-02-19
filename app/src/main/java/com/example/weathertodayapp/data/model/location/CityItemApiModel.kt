0ee21adfce9eb3807b86cc478a42afbe0fb812ce
Oralbek <theoralbeksapar@gmail.com>
2024-02-19 13:25:26 +0600

    minor changes

--- app/src/main/java/com/example/weathertodayapp/data/model/location/CityItemApiModel.kt
+++ app/src/main/java/com/example/weathertodayapp/data/model/location/CityItemApiModel.kt
@@ -1,12 +1,8 @@
 package com.example.weathertodayapp.data.model.location
 
-import com.google.gson.annotations.SerializedName
-
 data class CityItemApiModel(
     val country: String?,
     val lat: Double?,
-    @SerializedName("local_names")
-    val localNames: LocalNamesApiModel?,
     val lon: Double?,
     val name: String?,
     val state: String?
